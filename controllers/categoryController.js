9211919e104c8188c76f71ea08afbc736c00c22a
Danijel Stanic <danijel.stanich@gmail.com>
2021-08-27 11:41:55 +0200

    Set up the controller for handling post request on new category creation

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,6 +2,7 @@ var Category = require('../models/category');
 var Item = require('../models/item')
 
 var async = require('async')
+const { body,validationResult } = require("express-validator");
 
 var CL = require('./categoryList')
 
@@ -43,9 +44,41 @@ exports.category_create_get = function(req, res, next) {
 }
 
 // Handle form data to create a new Category
-exports.category_create_post = function(req, res) {
-    res.send('PAGE NOT IMPLEMENTED')
-}
+exports.category_create_post = [
+    body('name', 'Category name required').trim().isLength({min: 1}).escape(),
+    body('description').trim().escape(),
+
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a category object with escaped and trimmed data.
+        var category = new Category(
+            {   name: req.body.name,
+                description: req.body.description
+             });
+
+        if(!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/error messages.
+            console.log('errors detected')
+            async.parallel({
+                category_list: CL.get_category_list,
+            }, function(err, results) {
+                if(err) { return next(err)}
+
+                res.render('category_form', { title: 'Create a new Category' , category_list: results.category_list, category: category, errors: errors.array() })
+            })
+        } else {
+            // Data form is valid. Save category.
+            category.save(function(err) {
+                if(err) { return next(err) }
+                // Sucessful - redirect to the new category page
+                res.redirect(category.url)
+            })
+        }
+    }
+]
 
 // Display form for deleting an existing Category
 exports.category_delete_get = function(req, res) {
